8a01e7eb572a98eb87eeb14b96ad5bc128948c0e
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-03-01 10:17:38 +0100

    Some katas

--- codewars/003_01-03-23.sql
+++ codewars/003_01-03-23.sql
@@ -1,15 +1,15 @@
-// <7 kyu> SQL Basics: Raise to the Power
-// https://www.codewars.com/kata/594a8f653b5b4e8f3d000035
+-- <7 kyu> SQL Basics: Raise to the Power
+-- https://www.codewars.com/kata/594a8f653b5b4e8f3d000035
 SELECT POW(number1, number2) AS result FROM decimals;
-// <8 kyu> Is n divisible by x and y?
-// https://www.codewars.com/kata/5545f109004975ea66000086
+-- <8 kyu> Is n divisible by x and y?
+-- https://www.codewars.com/kata/5545f109004975ea66000086
 SELECT id,
 CASE WHEN (n % x = 0) AND (n % y = 0) THEN true
 ELSE false 
 END AS res
 FROM kata;
-// <7 kyu> SQL with Sailor Moon: Thinking about JOINs..
-// https://www.codewars.com/kata/5ab7a736edbcfc8e62000007
+-- <7 kyu> SQL with Sailor Moon: Thinking about JOINs..
+-- https://www.codewars.com/kata/5ab7a736edbcfc8e62000007
 SELECT
   senshi_name AS sailor_senshi,
   real_name_jpn AS real_name,
